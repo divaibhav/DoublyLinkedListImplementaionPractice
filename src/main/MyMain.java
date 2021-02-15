d0c7eea4c5ebee731bf6d6ea5643bedca8e910fd
Vaibhav Diwan <divaibhav@gmail.com>
2021-02-16 00:47:29 +0530

    Created MyMain.java

---
+++ src/main/MyMain.java
@@ -0,0 +1,8 @@
+package main;
+
+public class MyMain {
+    public static void main(String[] args) {
+        //create MyDoublyLinkedList instance and
+        // call all the method to test your implementation
+    }
+}
