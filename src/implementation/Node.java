840669dd806a3d49161ca61ee0382b79100808a6
Vaibhav Diwan <divaibhav@gmail.com>
2021-02-16 00:33:43 +0530

    declared Node class.

---
+++ src/implementation/Node.java
@@ -0,0 +1,10 @@
+package implementation;
+
+/**
+ * This class will contain doubly linked list node. Complete the class as per
+ * doubly linked list specification.
+ * You are free to override Object class methods and
+ * also free to implement any interface from java library.
+ */
+public class Node {
+}
