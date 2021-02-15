43928858977aa3e0029e11276b4a2a8de7938d08
Vaibhav Diwan <divaibhav@gmail.com>
2021-02-16 00:00:54 +0530

    declared removeFirst, removeLast, removeLastWithoutUsingTail and removeGivenNode(int givenElement) methods with specification

--- src/myinterface/DoublyLinkedListADT.java
+++ src/myinterface/DoublyLinkedListADT.java
@@ -31,4 +31,45 @@ public interface DoublyLinkedListADT {
      * @param givenElement it is the element after which we have to add the node
      */
     void addInBetween(int element, int givenElement);
+
+    /**
+     * This method will remove the first node of the linked list.
+     * It is also known as deleteFromHead or deleteFromBeginning.
+     *
+     * @return data of the first node, if list not empty, otherwise it will return 0,
+     * considering 0 as invalid data.
+     */
+    int removeFirst();
+
+    /**
+     * This method will remove the last node of the linked list.
+     * It is also known as deleteFromTail or deleteFromEnd.
+     * This method will use tail reference to delete the last node.
+     *
+     * @return data of the last node, if list not empty, otherwise it will return 0,
+     * considering 0 as invalid data.
+     */
+    int removeLast();
+
+    /**
+     * This method will remove the last node of the linked list.
+     * It is also known as deleteFromTail or deleteFromEnd.
+     * This method will not use tail reference to delete the last node.
+     * In this you have to traverse and identify the last node to delete
+     *
+     * @return data of the last node, if list not empty, otherwise it will return 0,
+     * considering 0 as invalid data.
+     */
+    int removeLastWithoutUsingTail();
+
+    /**
+     * This method will remove the given node, which contains the givenElement.
+     * If givenElement present in the list, it will delete the node containing givenElement
+     * otherwise it will return 0.
+     *
+     * @param givenElement the element of the given linked list
+     * @return will return the data of deleted node, if node present in list otherwise it will
+     * return 0, considering 0 as invalid data.
+     */
+    int removeGivenNode(int givenElement);
 }
